a85c4a4cb938a7061311fac0e86d5aa4c76d5cbf
Maks Verver <maksverver@geocities.com>
2009-06-01 22:42:16 +0200

    Updated tardiffinfo to print digests of data (i.e. non-diff) files too.

--- tardiffinfo.c
+++ tardiffinfo.c
@@ -1,9 +1,7 @@
 #include "common.h"
 
-static bool process_file(const char *path)
+static bool process_diff(InputStream *is)
 {
-    InputStream *is;
-    char        magic_buf[MAGIC_LEN];
     uint8_t     data[BS];
     uint32_t    S;
     uint16_t    C, A;
@@ -12,30 +10,12 @@ static bool process_file(const char *path)
     char        digest1_str[2*DS + 1];
     char        digest2_str[2*DS + 1];
 
-    printf("%s: ", path);
-    fflush(stdout);
-
-    is = (strcmp(path, "-") == 0) ? OpenStdinInputStream()
-                                  : OpenFileInputStream(path);
-    if (is == NULL)
-    {
-        printf("failed to open file\n");
-        goto failed;
-    }
-
-    if ( is->read(is, magic_buf, MAGIC_LEN) != MAGIC_LEN ||
-         memcmp(magic_buf, MAGIC_STR, MAGIC_LEN) != 0 )
-    {
-        printf("missing/invalid file signature\n");
-        goto failed;
-    }
-
     for (;;)
     {
         if (is->read(is, data, 8) != 8)
         {
             printf("read failed -- file truncated?\n");
-            goto failed;
+            return false;
         }
 
         S = parse_uint32(data + 0);
@@ -47,7 +27,7 @@ static bool process_file(const char *path)
         if (C >= 0x8000 || A >= 0x8000)
         {
             printf("invalid diff data\n");
-            goto failed;
+            return false;
         }
 
         while (A > 0)
@@ -55,7 +35,7 @@ static bool process_file(const char *path)
             if (is->read(is, data, BS) != BS)
             {
                 printf("read failed -- file truncated?\n");
-                goto failed;
+                return false;
             }
             A -= 1;
         }
@@ -64,7 +44,7 @@ static bool process_file(const char *path)
     if (is->read(is, digest1, DS) != DS)
     {
         printf("read failed -- file truncated?\n");
-        goto failed;
+        return false;
     }
     hexstring(digest1_str, digest1, DS);
 
@@ -75,18 +55,86 @@ static bool process_file(const char *path)
     }
     else
     {
+        /* Version 1.0 file; no input file digest present */
         strcpy(digest2_str, "?");
     }
 
     printf("%s -> %s\n", digest2_str, digest1_str);
-
-    /* successfully processed: */
-    is->close(is);
     return true;
+}
 
-failed:
-    if (is != NULL) is->close(is);
-    return false;
+static bool process_file(const char *path)
+{
+    bool        res;
+    InputStream *is;
+    char        buf[512];
+    size_t      len;
+
+    assert(sizeof(buf) >= MAGIC_LEN);
+
+    printf("%s: ", path);
+    fflush(stdout);
+
+    is = (strcmp(path, "-") == 0) ? OpenStdinInputStream()
+                                  : OpenFileInputStream(path);
+    if (is == NULL)
+    {
+        printf("failed to open file\n");
+        return false;
+    }
+
+    len = is->read(is, buf, MAGIC_LEN);
+    if (memcmp(buf, MAGIC_STR, len) == 0)
+    {
+        /* File starts with prefix of signature */
+        if (len < MAGIC_LEN)
+        {
+            if (len == 0)
+            {
+                printf("empty file\n");
+            }
+            else
+            {
+                printf("incomplete signature -- file truncated?\n");
+            }
+            return false;
+        }
+        else
+        {
+            /* Valid signature; process as differences file */
+            printf("diff: ");
+            fflush(stdout);
+            res = process_diff(is);
+        }
+    }
+    else
+    {
+        /* File does NOT start with a prefix of the signature; assume this is
+           not a differences file, but a regular data file instead. */
+        MD5_CTX     md5_ctx;
+        uint8_t     digest[DS];
+        char        digest_str[2*DS + 1];
+
+        printf("data: ");
+        fflush(stdout);
+
+        /* Compute MD5 hash of contents */
+        MD5_Init(&md5_ctx);
+        while (len > 0)
+        {
+            MD5_Update(&md5_ctx, buf, len);
+            len = is->read(is, buf, sizeof(buf));
+        }
+        MD5_Final(digest, &md5_ctx);
+
+        hexstring(digest_str, digest, DS);
+        printf("%s\n", digest_str);
+
+        res = true;
+    }
+
+    is->close(is);
+    return res;
 }
 
 int main(int argc, char *argv[])
